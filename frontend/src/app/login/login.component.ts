695eaea033501f790b3df04857a902d83f963c77
Jakob Klocker <jakobklocker@hotmail.com>
2023-09-15 10:48:04 +0200

    Feat: ip changed

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +10,6 @@ export class LoginComponent {
   constructor() {}
 
   login42(){
-    window.location.href = 'https://api.intra.42.fr/oauth/authorize' +
-    '?client_id=u-s4t2ud-9904fa10768d1a760e5ff38e9647bde2c6b9431a9c32b5269fe17946f41a414a' +
-    '&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2F42%2Fredirect' +
-    '&response_type=code';
+    window.location.href = environment.OAuthUrl;
   }
 }
