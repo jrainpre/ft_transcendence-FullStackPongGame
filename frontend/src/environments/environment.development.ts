b3c3f62b9ef62b9b861711e3d742771d3e5130e1
MabKort <kollermarcel@gmx.at>
2023-09-21 13:38:56 +0200

    sdaf

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl: `http://10.11.2.3:3001/api/`,
-    socketUrl: `http://10.11.2.3:3001`,
+    apiUrl: `http://10.11.2.5:3001/api/`,
+    socketUrl: `http://10.11.2.5:3001`,
     OAuthUrl: 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d2851e77be4a57685159f124605dce1179c5eb549d5694da732b438939b44222&redirect_uri=http%3A%2F%2F10.11.2.3%3A3001%2Fapi%2Fauth%2F42%2Fredirect&response_type=code',
 
 };
