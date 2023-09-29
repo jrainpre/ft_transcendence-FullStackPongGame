5e91b4b785dbbf8a4c9e0b3fdbfcbc3adeb80905
MichaelPerger <michaelperger@gmx.at>
2023-09-29 14:35:17 +0200

    fixed most of it

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: `http://10.11.2.3:3001/api/`,
-    socketUrl: `http://10.11.2.3:3001`,
+    apiUrl: `http://localhost:3001/api/`,
+    socketUrl: `http://localhost:3001`,
     OAuthUrl: 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-c5c9e8d1d5765cb168404ed369ed219dc1068fbd19e2189fae6c38328f5894ff&redirect_uri=http%3A%2F%2F10.11.2.3%3A3001%2Fapi%2Fauth%2F42%2Fredirect&response_type=code',
 };
